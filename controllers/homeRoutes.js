73bf11ad1963862aaf4f26c9783c9bee23e5f049
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2023-03-06 21:18:34 -0800

    fixed small things

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,90 @@
+// const router = require('express').Router();
+// const { Character, Post, User } = require('../models/');
+// const { format_date } = require('../utils/helper')
+// const sendEmail = require('../utils/sendEmail');
+// const withAuth = require("../utils/auth");
+
+
+// router.get('/', async (req, res) => {
+//     try {
+//       const postData = await Post.findAll({
+//         order: [['date_created', 'DESC']],
+//        include: [
+//         {
+//           model: User,
+//           attributes: ['email','battleTag']
+//         },
+//        ],
+//       });
+  
+//       const post = postData.map((post) => post.get({ plain: true }));
+//       res.render('homepage', { 
+//         post, loggedIn: req.session.loggedIn
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+// router.post('/send-email',async (req, res) => {
+//   const recipient = req.body.recipient;
+//   const subject = req.body.battleTag;
+//   const text = req.body.userMessage;
+
+//   try {
+//     await sendEmail(recipient, subject, text);
+//     res.status(200).json({message: 'Message sent!!'});
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+ 
+//   router.get('/post/:id', async (req, res) => {
+//     try {
+//       const postData = await Post.findOne({
+//         where: { id: req.params.id},
+//         include: [
+//           {model: User, attributes: ['battleTag']}
+//         ],
+//       });
+  
+//       if (postData) {
+//         const post = postData.get({ plain: true });
+  
+//       res.render('singlepost', {
+//         post,
+//       });
+//       } else {
+//         res.status(404).end()
+//       }
+  
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+
+
+// router.get('/login', (req, res) => {
+//     if (req.session.logged_in) {
+//       res.redirect('/');
+//       return;
+//     }
+//     res.render('login');
+//   });
+  
+//   router.get('/signup', (req, res) => {
+//     if (req.session.logged_in) {
+//       res.redirect('/');
+//       return;
+//     }
+  
+//     res.render('signup');
+//   });
+  
+
+// module.exports = router;
+
 const router = require('express').Router();
 const { Character, Post, User } = require('../models/');
 const { format_date } = require('../utils/helper')
